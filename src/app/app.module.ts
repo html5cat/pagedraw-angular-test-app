5d2da26f09361ebc8f20414638b8252c698ae935
Yuriy Dybskiy <yuriy@dybskiy.com>
2018-03-27 18:34:45 -0700

    Add test Pagedraw component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { PagedrawModule } from '../pagedraw/pagedraw.module';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PagedrawModule
   ],
   providers: [],
   bootstrap: [AppComponent]
